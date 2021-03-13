d1ad6a854d2dcd448cf5cd1332787776ad959b39
Nisharu <39903426+Nisharu@users.noreply.github.com>
2021-03-13 16:33:18 +0200

    Added Scene transfering to the wheel, New Wheel model

--- Assets/Scripts/Wheel/SpinTheWheel.cs
+++ Assets/Scripts/Wheel/SpinTheWheel.cs
@@ -3,42 +3,84 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 public class SpinTheWheel : MonoBehaviour
 {
 
     [SerializeField] private Rigidbody rigid_body;
     [SerializeField] private float spin_strenght;
 
-    float turn = -1;
-    [SerializeField] float timer = 0, max_time=0;
-    bool can_spin = true;
+    float turn;
+    [SerializeField] float timer;
+    bool is_spinning, was_active;
     // Start is called before the first frame update
     void Start()
     {
-
+        is_spinning = false;
+        was_active = false;
+        timer = 0;
+        turn = -1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && can_spin == true)
+        if(Input.GetKeyDown(KeyCode.Space) && is_spinning == false)
         {
-            WheelSpin();
-
+            SetTimer();
         }
+        WheelSpin();
     }
     void WheelSpin()
     {
-        can_spin = false;
-        max_time = UnityEngine.Random.Range(10, 100);
-        while (timer < max_time)
-        {                     
+        if (timer > 0)
+        {
             rigid_body.AddTorque(transform.up * spin_strenght * turn);
-           
-            timer += Time.deltaTime;
-        }
-        timer = 0;
-        can_spin = true;
 
+            timer -= Time.deltaTime;
+        }
+
+        if (timer < 0 && rigid_body.angularVelocity.y == 0)
+        {
+            
+            is_spinning = false;
+            Debug.Log(transform.localEulerAngles.y);
+            SelectLevel(transform.localEulerAngles.y);
+        }
+    }
+    void SetTimer()
+    {
+        is_spinning = true;
+        timer = UnityEngine.Random.Range(1, 10);            
+    }
+    void SelectLevel(float angle)
+    {
+        switch (angle)
+        {
+            case var n when (n >= 0 && n < 45):
+                SceneManager.LoadScene(1);
+                break;
+            case var n when (n >= 45 && n < 90):
+                SceneManager.LoadScene(2);
+                break;
+            case var n when (n >= 90 && n < 135):
+                SceneManager.LoadScene(3);
+                break;
+            case var n when (n >= 135 && n < 180):
+                SceneManager.LoadScene(4);
+                break;
+            case var n when (n >= 180 && n < 225):
+                SceneManager.LoadScene(5);
+                break;
+            case var n when (n >= 225 && n < 270):
+                SceneManager.LoadScene(6);
+                break;
+            case var n when (n >= 270 && n < 315):
+                SceneManager.LoadScene(7);
+                break;
+            case var n when (n >= 315 && n < 360):
+                SceneManager.LoadScene(8);
+                break;
+        }
     }
 }
