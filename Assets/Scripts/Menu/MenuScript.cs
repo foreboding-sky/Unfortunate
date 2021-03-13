295de0dcec7d787fb38cd80341e1567f6d566151
Nisharu <39903426+Nisharu@users.noreply.github.com>
2021-03-13 17:18:30 +0200

    Added Main menu

---
+++ Assets/Scripts/Menu/MenuScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+    public void Setting()
+    {
+
+    }
+    public void Exit()
+    {
+        Application.Quit();
+        
+    }
+}
