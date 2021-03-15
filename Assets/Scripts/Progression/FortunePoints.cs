292727e8683f75769fa8afa5c9b445449515c178
Nisharu <39903426+Nisharu@users.noreply.github.com>
2021-03-15 17:38:06 +0200

    Added Save system prototype

---
+++ Assets/Scripts/Progression/FortunePoints.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FortunePoints : MonoBehaviour, ISaveable
+{
+    [SerializeField] int fortune_points;
+    public object CaptureState()
+    {
+        return new SaveData
+        {
+            fortune_points = fortune_points
+        };
+    }
+
+    public void RestoreState(object state)
+    {
+        var saveData = (SaveData)state;
+        fortune_points = saveData.fortune_points;
+    }
+
+    [Serializable]
+    private struct SaveData
+    {
+        public int fortune_points;
+    }
+}
