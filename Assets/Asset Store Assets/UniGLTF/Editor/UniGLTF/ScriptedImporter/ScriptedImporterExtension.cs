d9e745aa54bc6963bc9f08e3cc8771dd200c0a05
Nisharu <39903426+Nisharu@users.noreply.github.com>
2021-03-21 18:06:09 +0200

    Player Model and animator

---
+++ Assets/Asset Store Assets/UniGLTF/Editor/UniGLTF/ScriptedImporter/ScriptedImporterExtension.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor.Experimental.AssetImporters;
+using UnityEditor;
+
+namespace UniGLTF
+{
+    public static class ScriptedImporterExtension
+    {
+        public static void ClearExternalObjects<T>(this ScriptedImporter importer) where T : UnityEngine.Object
+        {
+            foreach (var externalObject in importer.GetExternalObjectMap().Where(x => x.Key.type == typeof(T)))
+            {
+                importer.RemoveRemap(externalObject.Key);
+            }
+
+            AssetDatabase.WriteImportSettingsIfDirty(importer.assetPath);
+            AssetDatabase.ImportAsset(importer.assetPath, ImportAssetOptions.ForceUpdate);
+        }
+
+        public static IEnumerable<T> GetSubAssets<T>(this ScriptedImporter importer, string assetPath) where T : UnityEngine.Object
+        {
+            return AssetDatabase
+                .LoadAllAssetsAtPath(assetPath)
+                .Where(x => AssetDatabase.IsSubAsset(x))
+                .Where(x => x is T)
+                .Select(x => x as T);
+        }
+    }
+}
---
+++ Assets/Asset Store Assets/VRM/Editor/Format/VRMImporterMenu.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UniGLTF;
+using System;
+using System.Collections.Generic;
+
+namespace VRM
+{
+    public static class VRMImporterMenu
+    {
+        [MenuItem(VRMVersion.MENU + "/Import", priority = 1)]
+        static void ImportMenu()
+        {
+            var path = EditorUtility.OpenFilePanel("open vrm", "", "vrm");
+            if (string.IsNullOrEmpty(path))
+            {
+                return;
+            }
+
+            if (Application.isPlaying)
+            {
+                // load into scene
+                var parser = new GltfParser();
+                parser.ParsePath(path);
+
+                using (var context = new VRMImporterContext(parser))
+                {
+                    context.Load();
+                    context.EnableUpdateWhenOffscreen();
+                    context.ShowMeshes();
+                    context.DisposeOnGameObjectDestroyed();
+                    Selection.activeGameObject = context.Root;
+                }
+            }
+            else
+            {
+                if (path.StartsWithUnityAssetPath())
+                {
+                    Debug.LogWarningFormat("disallow import from folder under the Assets");
+                    return;
+                }
+
+                var assetPath = EditorUtility.SaveFilePanel("save prefab", "Assets", Path.GetFileNameWithoutExtension(path), "prefab");
+                if (string.IsNullOrEmpty(path))
+                {
+                    return;
+                }
+
+                if (!assetPath.StartsWithUnityAssetPath())
+                {
+                    Debug.LogWarningFormat("out of asset path: {0}", assetPath);
+                    return;
+                }
+
+                // import as asset
+                var prefabPath = UnityPath.FromUnityPath(assetPath);
+                var parser = new GltfParser();
+                parser.ParseGlb(File.ReadAllBytes(path));
+
+                Action<IEnumerable<string>> onCompleted = _ =>
+                {
+                    //
+                    // after textures imported
+                    //
+                    using (var context = new VRMImporterContext(parser))
+                    {
+                        var editor = new VRMEditorImporterContext(context, prefabPath);
+                        context.Load();
+                        editor.SaveAsAsset();
+                    }
+                };
+
+                using (var context = new VRMImporterContext(parser))
+                {
+                    var editor = new VRMEditorImporterContext(context, prefabPath);
+                    editor.ConvertAndExtractImages(UnityPath.FromFullpath(path), onCompleted);
+                }
+            }
+        }
+    }
+}
