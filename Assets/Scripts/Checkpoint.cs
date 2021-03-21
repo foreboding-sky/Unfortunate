8ffe20bfb33b85a3762d88eee2a31b9d51f8a6d0
Antao66 <46621433+Antao66@users.noreply.github.com>
2021-03-21 14:02:18 +0200

    level one checkpoints update 0.1

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,22 +4,31 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    public Transform checkpoint;
-    GameObject player;
+    public GameObject checkpoint;
+    //GameObject player;
     // Use this for initialization
     void Start()
     {
 
-        player = GameObject.FindWithTag("Player");
+        //player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void OnTriggerEnter(Collider plyr)
     {
-        if (plyr.gameObject.tag == "Player")
+        if (plyr.tag == "Player")
         {
-            player.transform.position = checkpoint.position;
-            player.transform.rotation = checkpoint.rotation;
+            Debug.Log(plyr.transform.position);
+            Debug.Log(plyr.transform.rotation);
+            Transform player = plyr.gameObject.transform.root.gameObject.GetComponent<Transform>();
+            //LifeSystem playerHealth = other.gameObject.transform.root.gameObject.GetComponent<LifeSystem>();
+            player.transform.position = checkpoint.transform.position;
+            player.transform.rotation = checkpoint.transform.rotation;
+            //player.transform.SetPositionAndRotation(checkpoint.transform.position, checkpoint.transform.rotation);
+            Debug.Log(checkpoint.transform.position);
+            Debug.Log(checkpoint.transform.rotation);
+            Debug.Log(player.transform.position);
+            Debug.Log(player.transform.rotation);
         }
     }
 }
