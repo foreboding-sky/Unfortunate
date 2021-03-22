35f66aa7c3ea2a768fbcd4783cc3b8a8488d083c
Nisharu <39903426+Nisharu@users.noreply.github.com>
2021-03-22 17:24:35 +0200

    )

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -22,13 +22,13 @@ public class PlayerStats : MonoBehaviour
     public int max_hearts = 3;
     public int curr_hearts = 6;
     public float dash_cd = 3f;
-    public float jump_force = 15f;
-    public int jump_number = 2;
-    public float gravity = 50f;
-    public float dash_speed = 50f;
+    public float jump_force = 1f;
+    public int jump_number = 1;
+    public float gravity = 2.3f;
+    public float dash_speed = 40;
     public float move_speed = 10f;
     public float rotation_speed = 50f;
-    public float start_dash_time = 0.2f;
+    public float start_dash_time = 0.3f;
 
 
 }
