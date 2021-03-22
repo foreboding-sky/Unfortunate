7c2ef712f9e5c870c69ec42ba8d2f6f33a4f1d4e
foreboding-sky <andriy.kondratuik@gmail.com>
2021-03-22 04:58:32 +0200

    MoViNg PlAtFoRm

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private List<Vector3> points;
+    [SerializeField] private float speed = 0.5f;
+    [SerializeField] private float waitTime = 1f;
+    private Rigidbody rb;
+    private bool isMoving = false;
+    private float startTime;
+    private Vector3 source;
+    private CharacterController player;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if (!isMoving)
+            StartCoroutine(MovePlatform());
+    }
+    IEnumerator MovePlatform()
+    {
+        isMoving = true;     
+        for (int i = 0; i < points.Count; i++)
+        {
+            startTime = Time.time;
+            source = transform.position;
+            while (Time.time < startTime + LerpTime(source, source + points[i]))
+            {
+                rb.MovePosition(transform.position + (points[i].normalized * Time.fixedDeltaTime * speed));
+                yield return new WaitForFixedUpdate();
+            }
+            yield return new WaitForSeconds(waitTime);
+        }
+
+        for (int i = points.Count - 1; i >= 0; i--)
+        {
+            startTime = Time.time;
+            source = transform.position;
+            while (Time.time < startTime + LerpTime(source, source + points[i]))
+            {
+                rb.MovePosition(transform.position - (points[i].normalized * Time.fixedDeltaTime * speed));
+                yield return new WaitForFixedUpdate();
+            }
+            yield return new WaitForSeconds(waitTime);
+        }
+        isMoving = false;
+        yield return null;
+    }
+    private float LerpTime(Vector3 first, Vector3 second)
+    {
+        float res = (first - second).magnitude;
+        return res / speed;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.GetComponent<CharacterController>())
+        {
+            player = other.gameObject.GetComponent<CharacterController>();
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.GetComponent<CharacterController>())
+        {
+            player.Move(rb.velocity * Time.deltaTime);
+        } 
+    }    
+}
